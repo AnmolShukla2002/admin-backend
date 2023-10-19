bfcde224c6074ff29afa6dcaa72d514bd26b9e22
Anmol Shukla <anmol.28422@gmail.com>
2023-10-19 14:43:22 +0530

    adding product image

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -2,8 +2,15 @@ const multer = require("multer");
 const sharp = require("sharp");
 const path = require("path");
 const fs = require("fs");
-
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/images/"));
+  },
+  filename: function (req, file, cb) {
+    const uniquesuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniquesuffix + ".jpeg");
+  },
+});
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image")) {
@@ -16,7 +23,7 @@ const multerFilter = (req, file, cb) => {
 const uploadPhoto = multer({
   storage: storage,
   fileFilter: multerFilter,
-  limits: { fileSize: 2000000 },
+  limits: { fileSize: 1000000 },
 });
 
 const productImgResize = async (req, res, next) => {
@@ -34,4 +41,18 @@ const productImgResize = async (req, res, next) => {
   next();
 };
 
-module.exports = { uploadPhoto, productImgResize };
+const blogImgResize = async (req, res, next) => {
+  if (!req.files) return next();
+  await Promise.all(
+    req.files.map(async (file) => {
+      await sharp(file.path)
+        .resize(300, 300)
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toFile(`public/images/blogs/${file.filename}`);
+      fs.unlinkSync(`public/images/blogs/${file.filename}`);
+    })
+  );
+  next();
+};
+module.exports = { uploadPhoto, productImgResize, blogImgResize };
